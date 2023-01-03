6f74dafd4fd153b9463a562ec252a5ba421a9855
Rishvic Pushpakaran <rishvic@gmail.com>
2023-01-04 00:26:54 +0530

    Fixed competition page scroll on Mobile

---
+++ public/js/competition/next-prev-buttons.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const pageCount = 17;
+const nextOnClicks = [];
+const prevOnClicks = [];
+
+function nextPage(page) {
+  return (page) % pageCount + 1;
+}
+function prevPage(page) {
+  return (page + pageCount - 2) % pageCount + 1;
+}
+
+for (let page = 1; page <= pageCount; page++) {
+  nextOnClicks[page] = function () {
+    document.querySelector(
+      `input[class="slide-radio${page}"]`
+    ).checked = false;
+    document.querySelector(
+      `input[class="slide-radio${nextPage(page)}"]`
+    ).checked = true;
+  };
+
+  prevOnClicks[page] = function () {
+    document.querySelector(
+      `input[class="slide-radio${page}"]`
+    ).checked = false;
+    document.querySelector(
+      `input[class="slide-radio${prevPage(page)}"]`
+    ).checked = true;
+  };
+}
